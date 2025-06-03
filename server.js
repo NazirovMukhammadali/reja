d005f6752b5e58a3784db6744d3b7e7aa706969f
Ali <nazirovmuhammadali@MacBook-Pro-Nazirov.local>
2025-06-03 13:59:30 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 //1
 app.use(express.static("public"));
@@ -10,17 +21,22 @@ app.use(express.urlencoded({ extended: true }));
 
 //2: Session
 
-//3
-app.set("views", "viwes");
+//3: Views code
+app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4
-app.get("/hello", function (req, res) {
-    res.end(`<h1>HELLO WORLD by Ali</h1>`);
+//4 Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "succes" });
 });
 
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sog'alar bo'limidasiz</h1>`);
+app.get('/author', (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 
